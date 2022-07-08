9abc863cfd976356fb75116b34dd5d8c1a18e4d5
JB Hutch <jbhutch01@gmail.com>
2022-07-08 09:55:45 -0400

    Uninstall flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
-};
+}
