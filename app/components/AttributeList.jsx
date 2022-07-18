f9f45e8626b53be5c54c7ebf7bbb46c711d02634
JB Hutch <jbhutch01@gmail.com>
2022-07-18 18:35:31 -0400

    More updates to attribute components

--- app/components/AttributeList.jsx
+++ app/components/AttributeList.jsx
@@ -33,6 +33,252 @@ const AttributeList = ({ onChange, attributes }) => {
           </div>
         )
 
+      case 'background-position':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={[
+                'top left',
+                'top center',
+                'top right',
+                'center left',
+                'center center',
+                'center right',
+                'bottom left',
+                'bottom center',
+                'bottom right',
+              ]}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'background-repeat':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={['repeat', 'repeat-x', 'repeat-y', 'no-repeat']}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'background-size':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={['auto', 'cover', 'contain']}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'background-url':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Input
+              type="url"
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'direction':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={['ltr', 'rtl']}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'font-style':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={['none', 'italic']}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'text-transform':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={['none', 'capitalize', 'lowercase', 'uppercase']}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'font-weight':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={[
+                '100',
+                '200',
+                '300',
+                '400',
+                '500',
+                '600',
+                '700',
+                '800',
+                '900',
+              ]}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'background-position-x':
+      case 'background-position-y':
       case 'border-top':
       case 'border-right':
       case 'border-bottom':
@@ -51,6 +297,118 @@ const AttributeList = ({ onChange, attributes }) => {
               label={val.label}
               value={val.value}
               defaultValue={val.defaultValue}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'target':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={['_self', '_blank']}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'text-align':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={['left', 'right', 'center']}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'text-decoration':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={['none', 'underline', 'line-through']}
+              onChange={(value) =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value,
+                  },
+                })
+              }
+              onBlur={() =>
+                onChange({
+                  [key]: {
+                    ...val,
+                    value: val.defaultValue,
+                  },
+                })
+              }
+            />
+          </div>
+        )
+
+      case 'vertical-align':
+        return (
+          <div key={idx} className="mb-2 w-full">
+            <Select
+              label={val.label}
+              value={val.value}
+              defaultValue={val.defaultValue}
+              options={['top', 'middle', 'bottom']}
               onChange={(value) =>
                 onChange({
                   [key]: {
--- app/components/Input.jsx
+++ app/components/Input.jsx
@@ -4,16 +4,16 @@ export default function Input({
   label,
   value,
   defaultValue,
+  type = 'text',
   onChange,
   onBlur,
 }) {
-  console.log({ value })
   return (
     <div>
       <label className="mb-2 text-sm font-medium text-gray-900">{label}</label>
       <input
         className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-        type="text"
+        type={type}
         defaultValue={defaultValue}
         value={value}
         onChange={(e) => onChange(e.target.value)}
