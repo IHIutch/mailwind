015c2fec66c4458171431fb926f824f3ca1a9b34
JB Hutch <jbhutch01@gmail.com>
2022-08-22 10:08:54 -0400

    Update quote block

--- app/components/blocks/QuoteBlock.jsx
+++ app/components/blocks/QuoteBlock.jsx
@@ -1,5 +1,17 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
+import Editor from '../Editor'
 
-export default function QuoteBlock({ details }) {
-  return <div>{JSON.stringify(details)}</div>
+export default function QuoteBlock({ details, onChange }) {
+  const handleChange = (value) => {
+    onChange({
+      ...details,
+      value,
+    })
+  }
+  return (
+    <Box borderLeftWidth="4px" borderColor="gray.200" pl="3" py="2">
+      <Editor value={details.value} onChange={handleChange} />
+    </Box>
+  )
 }
