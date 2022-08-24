4e879c67dfea9f8ce9d37a7b86b30a68f7076d96
JB Hutch <jbhutch01@gmail.com>
2022-08-24 09:54:31 -0400

    Some error handling

--- app/routes/download.jsx
+++ app/routes/download.jsx
@@ -1,13 +1,15 @@
+import { json } from '@remix-run/node'
 import getMjMl from '~/models/getMjml.server'
 
 export async function action({ request }) {
   const formData = await request.formData()
-  const json = formData.get('json')
+  const data = formData.get('json')
 
-  const { html, errors } = getMjMl(JSON.parse(json))
+  const { html, errors } = getMjMl(JSON.parse(data))
 
-  return {
-    html,
-    errors,
+  if (errors.length > 0) {
+    return json({ error: errors }, 400)
   }
+
+  return json({ html }, 200)
 }
