028a2f5c562743385565974add297e0c61a08fcd
JB Hutch <jbhutch01@gmail.com>
2023-01-26 16:10:53 -0500

    Install trpc and zod

--- utils/functions/index.ts
+++ utils/functions/index.ts
@@ -2,3 +2,16 @@ export function getErrorMessage(error: unknown) {
   if (error instanceof Error) return error.message
   return String(error)
 }
+
+export function getBaseUrl() {
+  if (typeof window !== 'undefined')
+    // browser should use relative path
+    return '';
+
+  if (process.env.VERCEL_URL)
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`;
+
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
