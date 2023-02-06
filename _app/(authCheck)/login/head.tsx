ed4f6a73f361b8afeebb0349644f4d3bca047142
JB Hutch <jbhutch01@gmail.com>
2023-02-05 23:32:28 -0500

    Delete _app dir

--- _app/(authCheck)/login/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head({ params }: { params: { slug: string } }) {
-  return (
-    <>
-      <title>Mailwind</title>
-      <meta name="description">Welcome to Mailwind!</meta>
-    </>
-  )
-}
--- _app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default function index() {
-  return (
-    <div>
-      <Link href="/login">Login</Link> <Link href="/demo">Demo</Link>
-    </div>
-  )
-}
